b53aff8b88d5002c84e264d8f956d215077192b2
feng <fre1994@gmail.com>
2017-12-10 10:02:33 -0700

    deleted useless functions

--- public/js/lessons.js
+++ public/js/lessons.js
@@ -8,30 +8,6 @@ const ajaxGetId = (id) => {
 }
 console.log(ajaxGetId(1))
 
-// const ajaxPost = () => {
-//   let insertData = JSON.stringify({
-//       user_client_id: "1",
-//       user_instructor_id: "2",
-//       cost: "$60",
-//       date_time: "12/23/17 1:00PM",
-//       lesson_name: "Intro to Electric Guitar",
-//       location: "CU School of Music",
-//     })
-//     console.log(insertData);
-//     console.log('Post route started');
-//   $.ajax({
-//     type: "POST",
-//     url: 'lessons',
-//     data:   insertData,
-//     success: (result) => {
-//       console.log(result)
-//       console.log('post success!');
-//     },
-//     dataType: 'application/json'
-//   });
-// }
-// console.log(ajaxPost())
-
 const sendInfo = {
   user_client_id: "1",
   user_instructor_id: "2",
@@ -58,7 +34,6 @@ function lessonPost(data) {
                    alert("Cannot add to list !");
                }
            },
-
            data: JSON.stringify(data)
        });
 }

ca05cdceebe61bab51580a2790e10204f754e749
feng <fre1994@gmail.com>
2017-12-11 12:43:57 -0700

    changed href for menu

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,11 @@
 const instructorFields = () => {
-  $('add_lessons').append(`<div class='row center'>
-        <button class="homebutton btn waves-effect waves-light" type="submit" name="action">Add Lessons</button>
-      </div>`)
+  const lessonsButton = `<div class='row home-menu center'>
+        <div class="center col s12 m6 l6">
+          <button class="home-menu btn waves-effect waves-light" type="submit" name="action">Add Lessons</button>
+        </div>
+      </div>`
+  return $('.add_lessons').append(lessonsButton)
 }
-
 const checkPrivileges = () => {
   if (id === 4){
     return instructorFields()

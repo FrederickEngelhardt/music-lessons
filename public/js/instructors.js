e2d6dc682ee3ecc54eeb5f1057f8a9480ec7845c
Ashley Amato <ashleyamato1@gmail.com>
2017-12-11 14:50:27 -0700

    append instructors to page

--- public/js/instructors.js
+++ public/js/instructors.js
@@ -1,28 +1,23 @@
-$.ajax({
-   headers : {
-        'Accept' : 'application/json',
-        'Content-Type' : 'application/json'
-    },
-   type: "GET",
-   url: "/users",
-   dataType: "json",
-   success: function (data) {
-     if (data) {
-       for (let i = 0; i < data.length; i++) {
-         if (data[i].skill_level_id === 4) {
-           console.log(data[i])
+$(document).ready(() => {
+  $.ajax({
+     headers : {
+          'Accept' : 'application/json',
+          'Content-Type' : 'application/json'
+      },
+     type: "GET",
+     url: "/users",
+     dataType: "json",
+     success: function (data) {
+       if (data) {
+         for (let i = 0; i < data.length; i++) {
+           if (data[i].skill_level_id === 4) {
+             console.log(data[i])
+             $('.instructor-list').append('<div>' + data[i].first_name + '</div>')
+           }
          }
+       } else {
+           alert("No teachers found.")
        }
-     } else {
-         alert("No teachers found.")
-     }
-   },
-})
-
-$(document).ready( () => {
-  console.log('page loaded');
-  $('button').click(function(event){
-    event.preventDefault()
-    createAccount()
+     },
   })
 })

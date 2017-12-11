9deea460625556d31301b76e62a0180b75e11723
Ashley Amato <ashleyamato1@gmail.com>
2017-12-11 15:53:04 -0700

    added instructor cards

--- public/js/instructors.js
+++ public/js/instructors.js
@@ -1,23 +1,41 @@
 $(document).ready(() => {
-  $.ajax({
-     headers : {
-          'Accept' : 'application/json',
-          'Content-Type' : 'application/json'
-      },
-     type: "GET",
-     url: "/users",
-     dataType: "json",
-     success: function (data) {
-       if (data) {
-         for (let i = 0; i < data.length; i++) {
-           if (data[i].skill_level_id === 4) {
-             console.log(data[i])
-             $('.instructor-list').append('<div>' + data[i].first_name + '</div>')
-           }
+$.ajax({
+   headers : {
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json'
+    },
+   type: "GET",
+   url: "/users",
+   dataType: "json",
+   success: function (data) {
+     if (data) {
+       for (let i = 0; i < data.length; i++) {
+         if (data[i].skill_level_id === 4) {
+           console.log(data[i])
+           $('.instructor-body').append(
+             `
+             <div class="row">
+               <div class="col s12 m7">
+                 <div class="instructor-card card">
+                   <div class="card-image">
+                     <img src="images/nigel.jpg">
+                     <span class="card-title black">${data[i].first_name}</span>
+                   </div>
+                   <div class="card-content">
+                     <p>${data[i].bio}.</p>
+                   </div>
+                   <div class="card-action">
+                     <a href="lessons.html">Book a lesson</a>
+                   </div>
+                 </div>
+               </div>
+             `
+           )
          }
-       } else {
-           alert("No teachers found.")
        }
-     },
-  })
+     } else {
+         alert("No teachers found.")
+     }
+   },
+})
 })

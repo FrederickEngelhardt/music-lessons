5cf0b9dcb929629cc1609755772e16a8385f3c17
Ashley Amato <ashleyamato1@gmail.com>
2017-12-12 15:15:13 -0700

    html and css changes to home, instructors, sign-in

--- public/js/instructors.js
+++ public/js/instructors.js
@@ -39,7 +39,15 @@ $.ajax({
    },
 })
   $(document).ready(() => {
-    $('.modal').modal();
-    $(".button-collapse").sideNav();
+    $('.modal').modal()
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 300
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true,
+        // onOpen: function(el) {}
+        // onClose: function(el) {}
+      }
+    );
   })
 })

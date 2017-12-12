b83211955daf5c43380fe830aedc3c5fe8690e0d
feng <fre1994@gmail.com>
2017-12-12 14:47:48 -0700

    added profile change forms. Fixed submit button

--- public/js/profile.js
+++ public/js/profile.js
@@ -18,31 +18,6 @@ const getAccount = () => {
     })
   })
 }
-
-const editAccount = (id, data) => {
-  $.ajax({
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    type: "PATCH",
-    url: `/users/${id}`,
-    dataType: "json",
-    success: function(msg) {
-      if (msg) {
-        console.log(`User ${id} was added in list !`);
-        // location.reload(true);
-        /* Activate this refresh when we hit submit.
-        even better way is:
-        $('#thisdiv').load(document.URL +  ' #thisdiv');
-         */
-      } else {
-        alert("Cannot add to list !")
-      }
-    },
-    data: JSON.stringify(data)
-  });
-}
 const editWindow = () => {
   const editCard = `<div class="card">
 
@@ -54,44 +29,69 @@ const editWindow = () => {
           <tr>
             <td>Phone Number</td>
             <td>
-              <input id="phone_number">
+              <input type="tel" id="phone_number">
             </td>
           </tr>
           <tr>
-            <td>Email</td>
-            <td id="email_address"></td>
-          </tr>
-          <tr>
             <td>Bio</td>
-            <td id="bio"></td>
+            <td>
+              <input type="text" id="bio">
+            </td>
           </tr>
         </tbody>
       </table>
       <div class="row center">
-        <button id="editButton" class="btn waves-effect waves-light orange" type="submit" name="action">Edit Profile</button>
+        <a href="./profile.html" id="submitButton" class="waves-effect waves-light btn">
+          <p class="login-button">Submit Changes</p>
+        </a>
       </div>
     </div>`
   $('#myProfile').remove()
   $('#profile_card').append(editCard)
-  console.log('clicked');
-  // $('#phone_number').append('')
-  // $('#email_address').html()
-  // $('#bio')
-
-
-
-  $.get('/token', result => {
-    const id = result.cookie.user_id
+  $('#submitButton').click(function(event) {
+    event.preventDefault()
+    submitEdit()
   })
-  // getAccount()
 }
+const submitEdit = () => {
+  let phone_number = $('#phone_number').val()
+  let bio = $('#bio').val()
+  let data = {phone_number,bio}
+  for (i in data){
+    if (data[i] === ''){
+      delete data[i]
+    }
+  }
+  $.get('/token', result => {
+    user_id = result.cookie.user_id
+  }).done( (result) => {
+    const id = result.cookie.user_id
+    $.ajax({
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      type: "PATCH",
+      url: `/users/${id}`,
+      dataType: "json",
+      success: function(msg) {
+        if (msg) {
+          console.log(`User information was successfully update!`);
+        } else {
+          alert("Cannot add to list.")
+        }
+      },
+      data: JSON.stringify(data)
+    });
+  })
+}
+
 $(document).ready(() => {
   console.log(getAccount())
   $('#editButton').click(function(event) {
     event.preventDefault()
     editWindow()
   })
-  // editWindow()
 })
 
 

76dd061c1f147fcb869ee959ac0d72479d2fb290
feng <fre1994@gmail.com>
2017-12-12 13:51:19 -0700

    can now remove the profile card and add edit card

---
+++ public/images/User profile_files/home.js
@@ -0,0 +1,60 @@
+(function() {
+  'use strict'
+
+  $.getJSON('/token')
+    .done(loggedIn => {
+      if (loggedIn) {
+        $('#logout').on('click', () => {
+          const options = {
+            dataType: 'json',
+            type: 'DELETE',
+            url: '/token'
+          }
+          $.ajax(options)
+            .done(() => {
+              window.location.href = '/index.html'
+            })
+            .fail(() => {
+              Materialize.toast('Unable to log out', 3000)
+            })
+        })
+      }
+    })
+
+})()
+
+const instructorFields = () => {
+  $('.add_lessons').append(`
+        <div class='row home-menu center'>
+          <div class="center col s12 m6 l6">
+            <a id="browse_instructors" class="home-button btn waves-effect waves-light" type="submit" name="action" href="instructors.html">
+            <text>Add Lesson</text></a>
+          </div>
+        </div>`)
+}
+
+const checkPrivileges = () => {
+  let skill_level
+  $.get('/token', result => {
+    const id = result.cookie.user_id
+    $.get(`/users/${id}`)
+      .done(data => {
+        skill_level = data.skill_level_id
+        if (skill_level == 4){return instructorFields()}
+        console.log(skill_level);
+      })
+  })
+}
+$(document).ready(() => {
+  checkPrivileges()
+  $('.modal').modal();
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true,
+      // onOpen: function(el) {}
+      // onClose: function(el) {}
+    }
+  );
+})
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,36 +1,97 @@
-const getAccount = (id) => {
-  $.get(`/users/${id}`).done( (data) => {
-    $('#first_name').append(data.first_name)
-    $('#last_name').append(data.last_name)
-    $('#phone_number').append(data.phone_number)
-    $('#skill_level_id').append(data.skill_level_id)
-    $('#bio').append(data.bio)
+var user_id
+
+function id() {
+  $.get('/token', result => {
+    user_id = result.cookie.user_id
   })
 }
-const modifyAccount = (id, data) => {
+const getAccount = () => {
+  $.get('/token', result => {
+    const id = result.cookie.user_id
+    $.get(`/users/${id}`).done((data) => {
+      $('#first_name').append(data.first_name)
+      $('#last_name').append(data.last_name)
+      $('#phone_number').append(data.phone_number)
+      $('#email_address').append(data.email_address)
+      $('#skill_level_id').append(data.skill_level_id)
+      $('#bio').append(data.bio)
+    })
+  })
+}
+
+const editAccount = (id, data) => {
   $.ajax({
-     headers : {
-          'Accept' : 'application/json',
-          'Content-Type' : 'application/json'
-      },
-     type: "PATCH",
-     url: `/users/${id}`,
-     dataType: "json",
-     success: function (msg) {
-         if (msg) {
-             console.log(`User ${id} was added in list !`);
-             // location.reload(true);
-             /* Activate this refresh when we hit submit.
-             even better way is:
-             $('#thisdiv').load(document.URL +  ' #thisdiv');
-              */
-         } else {alert("Cannot add to list !")}
-     },
-     data: JSON.stringify(data)
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    type: "PATCH",
+    url: `/users/${id}`,
+    dataType: "json",
+    success: function(msg) {
+      if (msg) {
+        console.log(`User ${id} was added in list !`);
+        // location.reload(true);
+        /* Activate this refresh when we hit submit.
+        even better way is:
+        $('#thisdiv').load(document.URL +  ' #thisdiv');
+         */
+      } else {
+        alert("Cannot add to list !")
+      }
+    },
+    data: JSON.stringify(data)
   });
 }
-$(document).ready( () => {
-  getAccount(1)
+const editWindow = () => {
+  const editCard = `<div class="card">
+
+      <table class="highlight">
+        <thead>
+          <h3>Edit Profile</h3>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Phone Number</td>
+            <td>
+              <input id="phone_number">
+            </td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td id="email_address"></td>
+          </tr>
+          <tr>
+            <td>Bio</td>
+            <td id="bio"></td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="row center">
+        <button id="editButton" class="btn waves-effect waves-light orange" type="submit" name="action">Edit Profile</button>
+      </div>
+    </div>`
+  $('#myProfile').remove()
+  $('#profile_card').append(editCard)
+  console.log('clicked');
+  // $('#phone_number').append('')
+  // $('#email_address').html()
+  // $('#bio')
+
+
+
+  $.get('/token', result => {
+    const id = result.cookie.user_id
+  })
+  // getAccount()
+}
+$(document).ready(() => {
+  console.log(getAccount())
+  $('#editButton').click(function(event) {
+    event.preventDefault()
+    editWindow()
+  })
+  // editWindow()
 })
 
 

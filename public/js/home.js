df3df61e3ad106ee67b41988c7f2204db699a578
feng <fre1994@gmail.com>
2017-12-12 20:57:04 -0700

    creates a new lesson with dynamic js

--- public/js/home.js
+++ public/js/home.js
@@ -36,7 +36,7 @@ const instructorFields = () => {
   $('.add_lessons').append(`
         <div class='row home-menu center'>
           <div class="center col s12 m6 l6">
-            <a id="create_lesson" class="home-button btn waves-effect waves-light" type="submit" name="action" href="#">
+            <a id="create_lesson" class="home-button btn waves-effect waves-light modal-trigger" type="submit" name="action" href="#new_lesson_modal">
             <text>Create Lesson</text></a>
           </div>
         </div>`)
@@ -49,12 +49,14 @@ const checkPrivileges = () => {
     $.get(`/users/${id}`)
       .done(data => {
         skill_level = data.skill_level_id
-        if (skill_level == 4){
+        if (skill_level == 4) {
           instructorFields()
           studentFields()
+          createLessonModal()
           return
+        } else {
+          return studentFields()
         }
-        else {return studentFields()}
       })
   })
 }
@@ -214,35 +216,128 @@ const createListeners = () => {
 /* Beginning of instructor lesson creation fields*/
 const createLesson = (data) => {
   if (!data) {
-
+    const lesson_name = $('#new_lesson_name').val()
+    const new_lesson_date = $('#new_lesson_date').val()
+    const new_lesson_time = $('#new_lesson_time').val()
+    const date_time = `${new_lesson_date} ${new_lesson_time}`
+    const location = $('#new_lesson_location').val()
+    const cost = $('#new_lesson_cost').val()
+    const instrument_type = $('#instrument_type').val()
+    $.get('/token').done ( result => {
+      console.log(result);
+      console.log(result.cookie.user_id);
+      data = {
+        user_client_id: null,
+        user_instructor_id: result.cookie.user_id,
+        lesson_name,
+        date_time,
+        location,
+        cost
+      }
+      console.log(data);
+      $.ajax({
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        type: "POST",
+        url: "/lessons",
+        dataType: "json",
+        success: function(msg) {
+          if (msg) {
+            console.log("Lesson" + sendInfo.lesson_name + " was added in list !");
+          } else {
+            alert("Cannot add to list !");
+          }
+        },
+        data: JSON.stringify(data)
+      }).done( (result) => {
+        alert('You just posted a lesson!')
+      })
+    })
   }
-  $.ajax({
-     headers : {
-          'Accept' : 'application/json',
-          'Content-Type' : 'application/json'
-      },
-     type: "POST",
-     url: "/lessons",
-     dataType: "json",
-     success: function (msg) {
-         if (msg) {
-             console.log("Lesson" + sendInfo.lesson_name + " was added in list !");
-         } else {
-             alert("Cannot add to list !");
-         }
-     },
-     data: JSON.stringify(data)
-  });
 }
-
-
-
-
-
-
-
-
-
+const createLessonModal = () => {
+  const html = `
+  <div id="new_lesson_modal" class="modal">
+    <div class="modal-content">
+      <div>
+        <table class="highlight">
+          <thead>
+            <h3>Create a Lesson</h3>
+          </thead>
+          <tbody>
+            <form action="alert('Lesson Created!')">
+              <tr class="row">
+                <td class="col s3 m3 l3">Lesson Name</td>
+                <td class="col s9 m9 l9">
+                  <input placeholder="Please enter lesson name" type="text" id="new_lesson_name" required="required">
+                </td>
+              </tr>
+              <tr class="row">
+                <td class="col s3 m3 l3">Date</td>
+                <td class="col s9 m9 l9">
+                  <input placeholder="enter date here" type="date" id="new_lesson_date" required="required">
+                </td>
+              </tr>
+              <tr class="row">
+                <td class="col s3 m3 l3">Time</td>
+                <td class="col s9 m9 l9">
+                  <input placeholder="enter time here" type="time" id="new_lesson_time" required="required">
+                </td>
+              </tr>
+              <tr class="row">
+                <td class="col s3 m3 l3">Location</td>
+                <td class="col s9 m9 l9">
+                  <input placeholder="please enter location of lesson" type="text" id="new_lesson_location" required="required">
+                </td>
+              </tr>
+              <tr class="row">
+                <td class="col s3 m3 l3">Lesson cost</td>
+                <td class="col s9 m9 l9">
+                  <input placeholder="enter lesson cost here" type="number" id="new_lesson_cost" required="required">
+                </td>
+              </tr>
+              <tr class="row">
+                <td class="col s3 m3 l3">Instrument type</td>
+                <td class="col s9 m9 l9">
+                  <input placeholder="Please enter instrument type" type="text" id="instrument_type">
+                </td>
+              </tr>
+            </form>
+          </tbody>
+        </table>
+        <div class="row center">
+          <a href="#" id="createLessonSubmitButton" class="waves-effect waves-light btn">
+            <p class="login-button">Create Lesson</p>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Back</a>
+    </div>
+  </div>
+    `
+  $('#create_lesson_modal_insert').html(html)
+  console.log('test');
+  // #NOTE you need this listener
+  $('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '4%', // Starting top style attribute
+    endingTop: '10%', // Ending top style attribute
+    // ready: function(modal, trigger) {
+    // },
+    // complete: function() {}
+  })
+  $('#createLessonSubmitButton').click( (event) =>{
+    event.preventDefault()
+    createLesson()
+  })
+}
 $(document).ready(() => {
   checkPrivileges()
   getAccount()
@@ -256,16 +351,14 @@ $(document).ready(() => {
     // ready: function(modal, trigger) {
     // },
     // complete: function() {}
-  }
-)
+  })
   $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 300
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true,
-      // onOpen: function(el) {}
-      // onClose: function(el) {}
-    }
-  )
+    menuWidth: 300, // Default is 300
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true,
+    // onOpen: function(el) {}
+    // onClose: function(el) {}
+  })
   createListeners()
 })

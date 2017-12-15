02e8857a443fc50bbcb1a332ba57186e8cd9fd8f
feng <fre1994@gmail.com>
2017-12-15 01:02:24 -0700

    fixed button text in my_lessons.html from spilling over.

--- db/seeds/02_users.js
+++ db/seeds/02_users.js
@@ -32,7 +32,7 @@ exports.seed = function(knex, Promise) {
       email_address: 'nigel.flippo@gmail.com',
       hashed_password: '$2a$10$zaDd/g677uKzQEU.Z1SyH.sP53Fu5VFzZQcQTrXx4si/QPI46c/zq',
       skill_level_id: 4,
-      user_avatar: 'guitarPlayer2.png',
+      user_avatar: 'nigel.png',
       created_at: new Date('2016-06-29 14:26:16 UTC'),
       updated_at: new Date('2016-06-29 14:26:16 UTC')
     },

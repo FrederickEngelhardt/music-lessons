ae2bc4925b4fdd79f566dcfb01dd140466679b52
feng <fre1994@gmail.com>
2017-12-15 00:06:58 -0700

    updated seeds to show pictures

--- db/migrations/03_user_avatar.js
+++ db/migrations/03_user_avatar.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.alterTable('users', function(table) {
-  table.string('user_avatar').notNullable().defaultTo('guitarPlayer2.png');
+  table.string('user_avatar').notNullable().defaultTo('guitarPlayer2.png')
 });
 };
 
